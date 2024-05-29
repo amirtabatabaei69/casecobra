4909a1142a4f8a9833f420665b4c641d7a5b6d46
amirtabatabaei69 <45861382+amirtabatabaei69@users.noreply.github.com>
2024-05-29 20:55:14 +0100

    initial commit

---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HandleComponent = () => {
+  return (
+    <div className="size-5 rounded-full shadow border bg-white border-zinc-900 transition hover:bg-primary" />
+  );
+};
+
+export default HandleComponent;
