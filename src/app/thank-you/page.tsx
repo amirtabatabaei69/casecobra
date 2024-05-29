2007b2dbce79417bed8e647197c3c8134eaab83c
amirtabatabaei69 <45861382+amirtabatabaei69@users.noreply.github.com>
2024-05-29 21:58:25 +0100

    dashboard page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react";
+import Thankyou from "./Thankyou";
+
+const Page = () => {
+  return (
+    <Suspense>
+      <Thankyou />
+    </Suspense>
+  );
+};
+
+export default Page;
